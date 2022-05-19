8a4892389821664b88db6d229f8307b3c043f4ed
yoshihide-saito <skyglowsy3@gmail.com>
2022-05-19 21:32:34 +0900

    fix travis file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,16 @@
 import colors from 'vuetify/es5/util/colors'
 
+// `DEPLOY_ENV` が `GH_PAGES` の場合のみ `router.base = '/<repository-name>/'` を追加する
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/<repository-name>/'
+  }
+} : {}
+
+module.exports = {
+  ...routerBase
+}
+
 export default {
   target: 'static',
 
